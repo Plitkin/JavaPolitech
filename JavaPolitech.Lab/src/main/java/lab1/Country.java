47de510279820de79f0b3566b5eaaca50f060900
Plitkin <plitkinva@gmail.com>
2023-10-20 22:00:50 +0300

    Add lab1

---
+++ JavaPolitech.Lab/src/main/java/lab1/Country.java
@@ -0,0 +1,116 @@
+package lab1;
+
+public class Country {
+    private String name;
+    private double area;
+    private double population;
+    private String capitalName;
+    private double capitalPopulation;
+
+    // Конструктор, принимающий значения всех атрибутов
+    public Country(String name, double area, double population, String capitalName, double capitalPopulation) {
+        if (area <= 0 || population <= 0 || name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Неверные данные для страны");
+        }
+        this.name = name;
+        this.area = area;
+        this.population = population;
+        this.capitalName = capitalName;
+        this.capitalPopulation = capitalPopulation;
+    }
+
+    // Конструктор для городов-государств
+    public Country(String name, double area, double population) {
+        this(name, area, population, "", 0);
+    }
+
+    // Геттеры
+    public String getName() {
+        return name;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getPopulation() {
+        return population;
+    }
+
+    public String getCapitalName() {
+        return capitalName;
+    }
+
+    public double getCapitalPopulation() {
+        return capitalPopulation;
+    }
+
+    // Геттер для плотности населения
+    public double getPopulationDensity() {
+        return population / area;
+    }
+
+    // Сеттеры
+    public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Название страны не может быть пустым");
+        }
+        this.name = name;
+    }
+
+    public void setArea(double area) {
+        if (area <= 0) {
+            throw new IllegalArgumentException("Площадь должна быть больше нуля");
+        }
+        this.area = area;
+    }
+
+    public void setPopulation(double population) {
+        if (population <= 0) {
+            throw new IllegalArgumentException("Население должно быть больше нуля");
+        }
+        this.population = population;
+    }
+
+    // Метод для задания/сброса данных о столице
+    public void setCapital(String capitalName, double capitalPopulation) {
+        this.capitalName = capitalName;
+        this.capitalPopulation = capitalPopulation;
+    }
+
+    public void resetCapital() {
+        this.capitalName = "";
+        this.capitalPopulation = 0;
+    }
+
+    // Метод для печати информации о стране
+    public void print() {
+        System.out.println("Название страны: " + name);
+        System.out.println("Площадь: " + area + " кв. км");
+        System.out.println("Население: " + population + " чел.");
+        if (!capitalName.isEmpty()) {
+            System.out.println("Столица: " + capitalName);
+            System.out.println("Население столицы: " + capitalPopulation + " чел.");
+        }
+        System.out.println("Плотность населения: " + getPopulationDensity() + " чел./кв. км");
+    }
+
+    // Статический метод для печати информации о массиве стран
+    public static void printAll(Country[] countries) {
+        for (Country country : countries) {
+            country.print();
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Country[] countries = new Country[5];
+        countries[0] = new Country("Russia", 17.1e6, 146.7e6, "Moscow", 12.6e6);
+        countries[1] = new Country("Finland", 338e3, 5.5e6, "Helsinki", 655e3);
+        countries[2] = new Country("France", 643.8e3, 67.8e6, "Paris", 2.1e6);
+        countries[3] = new Country("Andorra", 467e3, 85.4e3, "Andorra la Vella", 22.6e3);
+        countries[4] = new Country("Singapore", 725e3, 5.7e6);
+
+        Country.printAll(countries);
+    }
+}
